76741d9d42a9b0c00e767dd99a5a2afc33aa9619
devlaka <lakshanwarunab@gmail.com>
2022-04-27 16:51:09 +0530

    add images and update accommodation model

--- models/accommodation.js
+++ models/accommodation.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const accommodationSchema = new mongoose.Schema({
   title: String,
-  price: String,
+  image: String,
+  price: Number,
   description: String,
   location: String,
 });
--- seeds/index.js
+++ seeds/index.js
@@ -14,9 +14,14 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   await Accommodation.deleteMany({});
   for (let i = 0; i < 50; i++) {
+    const price = Math.floor(Math.random() * 50);
     const accommodation = new Accommodation({
       location: `${sample(cities).city}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut, nisi! Quae, eos mollitia quas quasi sapiente commodi, illo quia a quo nihil eaque dicta sint ad blanditiis culpa adipisci ullam.",
+      price,
     });
     await accommodation.save();
   }
